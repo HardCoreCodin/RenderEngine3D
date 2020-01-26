39500c962eacda1e9d3b986c04e6118d41b0ddee
Arnon Marcus <a.m.marcus@gmail.com>
2020-01-26 19:05:55 +1300

    More cleanups to viewport rendering, cleaned up the test script

--- engine/testing/main2.ts
+++ engine/testing/main2.ts
@@ -4,65 +4,74 @@ import {ATTRIBUTE, FACE_TYPE} from "../constants.js";
 import {MeshOptions} from "../lib/geometry/options.js";
 
 import GLRenderEngine from "../lib/render/raster/hardware/engine.js";
+import {rgba} from "../lib/accessors/color.js";
 
-var g: {[k: string]: any} = {};
-globalThis.g = g;
+globalThis.rgba = rgba;
+globalThis.r = rgba(1, 0 ,0, 1);
+globalThis.g = rgba(0, 1 ,0, 1);
+globalThis.b = rgba(0, 0 ,1, 1);
 
-const engine = g.engine = new GLRenderEngine();
-const camera = g.c = engine.display.active_viewport.controller.camera;
+const engine = globalThis.engine = new GLRenderEngine();
+const mesh_options = new MeshOptions(1, 0,0, true);
+const mesh_inputs = new MeshInputs(FACE_TYPE.TRIANGLE, ATTRIBUTE.position|ATTRIBUTE.uv);
+const input_positions = mesh_inputs.position;
+const input_uvs = mesh_inputs.uv;
+
+const display = engine.display;
+const vp1 = display.active_viewport;
+const camera = vp1.controller.camera;
 camera.is_static = false;
 camera.lense.fov = 75;
 camera.transform.translation.y = 1;
 
 
-
-g.mesh_options = new MeshOptions(1, 0,0, true);
-g.mesh_inputs = new MeshInputs(FACE_TYPE.TRIANGLE, ATTRIBUTE.position|ATTRIBUTE.uv);
-g.input_positions = g.mesh_inputs.position;
-g.input_uvs = g.mesh_inputs.uv;
-
 (async () => {
-    g.susan = (await (await fetch('Susan.json')).json()).meshes[0];
-    g.susan_positions = g.susan.vertices;
-    g.susan_uvs = g.susan.texturecoords[0];
-    g.vertex_count = g.susan_positions.length / 3;
+    const susan = (await (await fetch('Susan.json')).json()).meshes[0];
+    const susan_positions = susan.vertices;
+    const susan_uvs = susan.texturecoords[0];
+    const vertex_count = susan_positions.length / 3;
 
     const vertex_uv: [number, number] = [0, 0];
     const vertex_position: [number, number, number] = [0, 0, 0];
 
     let vertex_uv_offset = 0;
     let vertex_position_offset = 0;
-    for (let vertex_id = 0; vertex_id < g.vertex_count; vertex_id++) {
-        vertex_position[0] = g.susan_positions[vertex_position_offset];
-        vertex_position[1] = g.susan_positions[vertex_position_offset + 1];
-        vertex_position[2] = g.susan_positions[vertex_position_offset + 2];
-        g.input_positions.pushVertex(vertex_position);
+    for (let vertex_id = 0; vertex_id < vertex_count; vertex_id++) {
+        vertex_position[0] = susan_positions[vertex_position_offset];
+        vertex_position[1] = susan_positions[vertex_position_offset + 1];
+        vertex_position[2] = susan_positions[vertex_position_offset + 2];
+        input_positions.pushVertex(vertex_position);
 
-        vertex_uv[0] = g.susan_uvs[vertex_uv_offset];
-        vertex_uv[1] = g.susan_uvs[vertex_uv_offset + 1];
-        g.input_uvs.pushVertex(vertex_uv);
+        vertex_uv[0] = susan_uvs[vertex_uv_offset];
+        vertex_uv[1] = susan_uvs[vertex_uv_offset + 1];
+        input_uvs.pushVertex(vertex_uv);
 
         vertex_uv_offset += 2;
         vertex_position_offset += 3;
     }
-    for (const face of g.susan.faces)
-        g.input_positions.pushFace(face);
+    for (const face of susan.faces)
+        input_positions.pushFace(face);
 
-    g.mesh = new Mesh(g.mesh_inputs, g.mesh_options).load();
+    const mesh = new Mesh(mesh_inputs, mesh_options).load();
     // g.geometry = g.engine.scene.mesh_geometries.addGeometry(g.mesh);
     // g.geometry.material = g.engine.scene.addMaterial(GLMaterial);
 
     for (let i=0; i < 10; i++)
         for (let j=0; j< 10; j++) {
-            let geo = g.engine.scene.mesh_geometries.addGeometry(g.mesh);
+            let geo = engine.scene.mesh_geometries.addGeometry(mesh);
             geo.transform.translation.x = i - 5;
             geo.transform.translation.z = j - 5;
             geo.transform.scale.x = geo.transform.scale.y = geo.transform.scale.z = 0.1
         }
 
 
-    g.engine.start();
+    engine.start();
 
-    g.engine.display.addViewport(g.engine.display.active_viewport.controller);
-    g.engine.display.addViewport(g.engine.display.active_viewport.controller);
+    // display = engine.display;
+    // vp1 = display.active_viewport;
+    // vp2 = display.addViewport(vp1.controller);
+    // vp3 = display.addViewport(vp1.controller);
+
 })();
+
+// globalThis.g = g;
